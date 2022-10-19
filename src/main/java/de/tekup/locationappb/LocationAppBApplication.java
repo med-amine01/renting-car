8a31e255d7099e33b1a85a177c6f23afcf45c5a5
Hmida-Rojbani <hmida.rojbani@fst.utm.tn>
2022-10-19 12:15:13 +0100

    config h2

---
+++ src/main/java/de/tekup/locationappb/LocationAppBApplication.java
@@ -0,0 +1,13 @@
+package de.tekup.locationappb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationAppBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocationAppBApplication.class, args);
+	}
+
+}
