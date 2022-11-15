1cb1ecbf1b325d3d2d59c943e203f306df6b2f8d
Hmida-Rojbani <hmida.rojbani@fst.utm.tn>
2022-11-15 11:31:07 +0100

    connect to DB and display clients

--- src/main/java/de/tekup/locationappb/controllers/ViewClientsCtrl.java
+++ src/main/java/de/tekup/locationappb/controllers/ViewClientsCtrl.java
@@ -1,14 +1,22 @@
 package de.tekup.locationappb.controllers;
 
+import de.tekup.locationappb.services.ClientService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/clients/ui")
+@AllArgsConstructor
 public class ViewClientsCtrl {
+
+    private ClientService clientService;
+
     @GetMapping("/display")
-    public String displayClients(){
+    public String displayClients(Model model){
+        model.addAttribute("clients",clientService.getAll());
         return "clients-display";
     }
 }
